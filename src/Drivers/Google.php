0984653627869df12cd6bfd1dd606529c86c031e
Radu <radu.bordei89@gmail.com>
2015-08-20 14:24:22 +0300

    rm.

--- src/Drivers/Google.php
+++ src/Drivers/Google.php
@@ -7,19 +7,4 @@ use Translator\Translatable;
 
 class Google extends Driver implements Translatable {
 
-    public function get($key, $replacement = array(), $locale = null) {
-        // TODO: Implement get() method.
-    }
-
-    public function has($key) {
-        // TODO: Implement has() method.
-    }
-
-    public function delete($key, $locale = null) {
-        // TODO: Implement delete() method.
-    }
-
-    public function translate($key, $translation, $locale = null) {
-        // TODO: Implement translate() method.
-    }
 }
