b47ee27eee36d1499f3386693345061947f875cb
Radu <radu.bordei89@gmail.com>
2015-08-20 14:24:13 +0300

    silent call function.

--- src/Driver.php
+++ src/Driver.php
@@ -11,4 +11,49 @@ abstract class Driver {
     public function __construct($attributes = array()) {
         $this->setAttributes($attributes);
     }
+
+    /**
+     * Get translation by key .
+     *
+     * @param $key
+     * @param array $replacement
+     * @param null $locale
+     * @return bool
+     */
+    public function get($key, $replacement = array(), $locale = null) {
+        return true;
+    }
+
+    /**
+     * Check if has translation by key .
+     *
+     * @param $key
+     * @return bool
+     */
+    public function has($key) {
+        return true;
+    }
+
+    /**
+     * Delete translation by key .
+     *
+     * @param $key
+     * @param null $locale
+     * @return bool
+     */
+    public function delete($key, $locale = null) {
+        return true;
+    }
+
+    /**
+     * Translate key .
+     *
+     * @param $key
+     * @param $translation
+     * @param null $locale
+     * @return bool
+     */
+    public function translate($key, $translation, $locale = null) {
+        return true;
+    }
 }
