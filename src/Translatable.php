d076a7e519bba77f3cb7ddc217c3d0bc961d1135
Radu <radu.bordei89@gmail.com>
2015-08-20 15:07:08 +0300

    adding comments.

--- src/Translatable.php
+++ src/Translatable.php
@@ -4,11 +4,40 @@ namespace Translator;
 
 interface Translatable {
 
+    /**
+     * Get translation by key .
+     *
+     * @param $key
+     * @param array $replacement
+     * @param null $locale
+     * @return mixed
+     */
     public function get($key, $replacement = array(), $locale = null);
 
+    /**
+     * Check if has translation by key .
+     *
+     * @param $key
+     * @return mixed
+     */
     public function has($key);
 
+    /**
+     * Delete translation by key .
+     *
+     * @param $key
+     * @param null $locale
+     * @return mixed
+     */
     public function delete($key, $locale = null);
 
+    /**
+     * Translate translation by key .
+     *
+     * @param $key
+     * @param $translation
+     * @param null $locale
+     * @return mixed
+     */
     public function translate($key, $translation, $locale = null);
 }
