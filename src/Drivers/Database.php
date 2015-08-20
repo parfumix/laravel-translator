910531685287f94da5968adec248e5b6da755239
Radu <radu.bordei89@gmail.com>
2015-08-20 11:19:10 +0300

    adding drivers.

---
+++ src/Drivers/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Translator\Drivers;
+
+use Translator\Translatable;
+
+class Database implements Translatable {
+
+    public function get($key, $replacement = array(), $locale = null) {
+        // TODO: Implement get() method.
+    }
+
+    public function has($key) {
+        // TODO: Implement has() method.
+    }
+
+    public function delete($key, $locale = null) {
+        // TODO: Implement delete() method.
+    }
+
+    public function translate($key, $translation, $locale = null) {
+        // TODO: Implement translate() method.
+    }
+}
