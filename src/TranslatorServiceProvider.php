40a386a7714c78e88c0def243d0ddc949c2ca9e4
Radu <radu.bordei89@gmail.com>
2015-08-20 11:21:08 +0300

    improve translatorServiceProvider class.

--- src/TranslatorServiceProvider.php
+++ src/TranslatorServiceProvider.php
@@ -1,7 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: radu
- * Date: 8/20/15
- * Time: 10:26 AM
- */
+
+namespace Translator;
+
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Yaml\Yaml;
+
+class TranslatorServiceProvider extends ServiceProvider {
+
+    public function boot() {
+        $this->loadConfiguration();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+
+        /** Register driver manager */
+        $this->app->bind('driver-translator-manager', function() {
+            return (new Manager(
+                config('laravel-translator')
+            ));
+        });
+
+        /** Register translator . */
+        $this->app->bind('translator', function($app) {
+            return new Translator(
+                config('laravel-translator'),
+                $app['driver-translator-manager']
+            );
+        });
+    }
+
+    /**
+     * Load conf .
+     *
+     * @return $this
+     */
+    protected function loadConfiguration() {
+        $array = Yaml::parse(file_get_contents(
+            __DIR__ . '/../configuration/general.yaml'
+        ));
+
+        $config = $this->app['config']->get('laravel-translator', []);
+
+        $this->app['config']->set('laravel-translator', array_merge($array, $config));
+
+        return $this;
+    }
+}
