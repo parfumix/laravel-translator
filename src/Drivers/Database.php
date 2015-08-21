ae58e944a77c5d3a282be6135996fa8ef39b8c65
Radu <radu.bordei89@gmail.com>
2015-08-21 12:13:05 +0300

    improve database driver.

--- src/Drivers/Database.php
+++ src/Drivers/Database.php
@@ -2,13 +2,17 @@
 
 namespace Translator\Drivers;
 
+use Illuminate\Support\Facades\Cache;
 use Translator\Driver;
-use Translator\DriverAssets\Database\Language;
 use Translator\DriverAssets\Database\LanguageRepository;
 use Translator\DriverAssets\Database\Translation;
 use Translator\DriverAssets\Database\TranslationsRepository;
 use Translator\Translatable;
 
+/**
+ * Class Database
+ * @package Translator\Drivers
+ */
 class Database extends Driver implements Translatable {
 
     /**
@@ -16,11 +20,21 @@ class Database extends Driver implements Translatable {
      */
     protected $translationRepository;
 
-    public function __construct() {
+    /**
+     * @var
+     */
+    protected $translations;
+
+    public function __construct($attributes = array()) {
+        parent::__construct($attributes);
+
         $this->translationRepository = new TranslationsRepository(
             new Translation()
         );
+
+        $this->setTranslations();
     }
+
 
     /**
      * Get repository .
@@ -43,6 +57,77 @@ class Database extends Driver implements Translatable {
         return $this;
     }
 
+
+    /**
+     * Set translations .
+     *
+     * @return $this
+     */
+    protected function setTranslations() {
+        if( $this->getAttribute('cache_time') ) {
+            $translations = Cache::remember('users', $this->getAttribute('cache_time'), function() {
+                $translations = $this->getRepository()
+                    ->allTranslations()
+                    ->toArray();
+
+                return $this->prepareTranslations(
+                    $translations
+                );
+            });
+        } else {
+            $translations = $this->getRepository()
+                ->allTranslations()
+                ->toArray();
+
+            $translations = $this->prepareTranslations(
+                $translations
+            );
+        }
+
+        $this->translations = $translations;
+
+        return $this;
+    }
+
+    /**
+     * Prepare translations .
+     *
+     * @param $translations
+     * @return array
+     */
+    protected function prepareTranslations($translations) {
+        $return = [];
+
+        foreach($translations as $translation)
+            $return[$translation['locale']][!is_null($translation['group']) ? $translation['group']. '.' . $translation['key'] : ''. $translation['key']] = $translation['value'];
+
+       return $return;
+    }
+
+    /**
+     * Get all translations ..
+     *
+     * @return mixed
+     */
+    protected function getTranslations() {
+        return $this->translations;
+    }
+
+    /**
+     * Get translation by key .
+     *
+     * @param $key
+     * @param $locale
+     * @return mixed
+     */
+    protected function getTranslation($key, $locale) {
+        if( isset($this->translations[$locale][$key]) )
+            return $this->translations[$locale][$key];
+
+        return $key;
+    }
+
+
     /**
      * Get translation by key .
      *
@@ -52,24 +137,56 @@ class Database extends Driver implements Translatable {
      * @return bool
      */
     public function get($key, $replacement = array(), $locale = null) {
-        $locale = ! is_null($locale) ? $locale : $this->locale();
+        $locale = $this->locale($locale);
 
-        $translation = $this->getRepository()
-            ->getByKey($key, $locale);
-
-        if( isset($translation->value) )
-            return $translation->value;
-
-        return $key;
+        return $this->getTranslation($key, $locale);
     }
 
     /**
      * Check if has translation .
      *
      * @param $key
+     * @param null $locale
      * @return bool
      */
-    public function has($key) {
-        return ! is_null($this->get($key));
+    public function has($key, $locale = null) {
+        $locale = $this->locale($locale);
+
+        return $this->get($key, [], $locale) !== $key;
     }
+
+    /**
+     * Delete translation by key .
+     *
+     * @param $key
+     * @param null $group
+     * @param null $locale
+     * @return bool
+     */
+    public function delete($key, $group = null, $locale = null) {
+        $locale = $this->locale($locale);
+
+        $translation = $this->getRepository()
+            ->getByKey($key, $locale, $group);
+
+        if( isset($translation->id) )
+            $this->getRepository()
+                ->removeById($translation->id);
+
+        return true;
+    }
+
+    /**
+     * Translate key .
+     *
+     * @param $key
+     * @param $translation
+     * @param null $locale
+     * @return bool
+     */
+    public function translate($key, $translation, $locale = null) {
+        
+        return true;
+    }
+
 }
